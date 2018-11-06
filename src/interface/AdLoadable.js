2d9ec6d012a389d2bb699930a534f7cec79059b3
alextremp <alex.castells@schibsted.com>
2018-11-06 14:40:26 +0100

    Add minimum required parameter to specification object

--- src/interface/AdLoadable.js
+++ src/interface/AdLoadable.js
@@ -6,6 +6,7 @@ export default class AdLoadable {
    * Returns the loaded Ad from AdServer
    * @param {string} id - the Ad unique identifier
    * @param {Object} specification - Ad's connector specific data required to load it
+   * @param {string} specification.source - connector source
    * @returns {Promise} Promise object representing when the operation finish
    */
   loadAd ({id, specification}) {
